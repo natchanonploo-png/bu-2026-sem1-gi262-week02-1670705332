b6df48e71adad3d80d06980e92c270ea1ac4c1e1
natchanonploo-png <natchanon.ploo@bumail.net>
2026-09-02 18:52:06 +0700

    AS1-AS16

--- Assets/Workshop/Student/Scripts/Assignment.cs
+++ Assets/Workshop/Student/Scripts/Assignment.cs
@@ -46,7 +46,9 @@ namespace Assignment
         public GameObject[] as01_items;
         public void AS01_RandomItemDrop()
         {
-            throw new NotImplementedException();
+            int r = UnityEngine.Random.Range(0, as01_items.Length);
+            GameObject go = Instantiate(as01_items[r]);
+            Debug.Log($"Got item: {go.name}");
         }
 
         /*
@@ -106,9 +108,21 @@ namespace Assignment
         public GameObject[] as02_floorTiles;
         public int as02_columns;
         public int as02_rows;
+
         public void AS02_NestedLoopForCreate2DMap()
         {
-            throw new NotImplementedException();
+            for (int y = 0; y < as02_rows; y++)
+            {
+                string rowPattern = "";
+                for (int x = 0; x < as02_columns; x++)
+                {
+                    int r = UnityEngine.Random.Range(0, as02_floorTiles.Length);
+                    GameObject tile = Instantiate(as02_floorTiles[r], new Vector2(x, y), transform.rotation);
+                    rowPattern += tile.name;
+                }
+
+                Debug.Log(rowPattern);
+            }
         }
 
         /*
@@ -202,7 +216,16 @@ namespace Assignment
         public int as03_rows;
         public void AS03_NestedLoopForMakingWallAround()
         {
-            throw new NotImplementedException();
+            for (int y = 0; y < as03_rows; y++)
+            {
+                for (int x = 0; x < as03_columns; x++)
+                {
+                    if (x == 0 || x == as03_columns - 1 || y == 0 || y == as03_rows - 1)
+                    {
+                        Instantiate(as03_wall, new Vector2(x, y), transform.rotation);
+                    }
+                }
+            }
         }
 
         /*
@@ -237,7 +260,18 @@ namespace Assignment
         public int as04_target;
         public void AS04_AttackEnemy()
         {
-            throw new NotImplementedException();
+            as04_enemyHP[0] -= as04_damage;
+            if (as04_enemyHP[0] < 0) as04_enemyHP[0] = 0;
+            Debug.Log($"FirstEnemy hp : {as04_enemyHP[0]}");
+            
+            int lastIndex = as04_enemyHP.Length - 1;
+            as04_enemyHP[lastIndex] -= as04_damage;
+            if (as04_enemyHP[lastIndex] < 0) as04_enemyHP[lastIndex] = 0;
+            Debug.Log($"LastEnemy hp : {as04_enemyHP[lastIndex]}");
+            
+            as04_enemyHP[as04_target] -= as04_damage;
+            if (as04_enemyHP[as04_target] < 0) as04_enemyHP[as04_target] = 0;
+            Debug.Log($"TargetEnemy {as04_target} hp : {as04_enemyHP[as04_target]}");
         }
 
         /*
@@ -260,7 +294,10 @@ namespace Assignment
         public int as05_n;
         public void AS05_DynamicIterationLoop()
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < as05_n; i++)
+            {
+                Debug.Log(i);
+            }
         }
 
         /*
@@ -309,7 +346,21 @@ namespace Assignment
         public string[] as06_ironManSuitNames;
         public void AS06_WhileLoopAndArray()
         {
-            throw new NotImplementedException();
+            Debug.Log("======Log by One======");
+            int i = 0;
+            while (i < as06_ironManSuitNames.Length)
+            {
+                Debug.Log(as06_ironManSuitNames[i]);
+                i += 1;
+            }
+
+            Debug.Log("======Log by Two======");
+            int j = 0;
+            while (j < as06_ironManSuitNames.Length)
+            {
+                Debug.Log(as06_ironManSuitNames[j]);
+                j += 2;
+            }
         }
 
         /*
@@ -347,7 +398,15 @@ namespace Assignment
         public int as07_targetIndex;
         public void AS07_HealTargetAtIndex()
         {
-            throw new NotImplementedException();
+            as07_heroHPs[0] += as07_heal;
+            Debug.Log($"FirstHero hp : {as07_heroHPs[0]}");
+            
+            int lastIndex = as07_heroHPs.Length - 1;
+            as07_heroHPs[lastIndex] += as07_heal;
+            Debug.Log($"LastHero hp : {as07_heroHPs[lastIndex]}");
+            
+            as07_heroHPs[as07_targetIndex] += as07_heal;
+            Debug.Log($"TargetHero {as07_targetIndex} hp : {as07_heroHPs[as07_targetIndex]}");
         }
 
         /*
@@ -374,8 +433,8 @@ namespace Assignment
         public string[] as08_dialogues;
         public void AS08_RandomPickingDialogue()
         {
-            throw new NotImplementedException();
-        }
+            int r = UnityEngine.Random.Range(0, as08_dialogues.Length);
+            Debug.Log(as08_dialogues[r]);        }
 
         /*
          * จงเขียนโปรแกรมเพื่อสร้างตารางสูตรคูณ จาก 1 - 12
@@ -400,7 +459,11 @@ namespace Assignment
         public int as09_n;
         public void AS09_MultiplicationTable()
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= 12; i++)
+            {
+                Debug.Log($"{as09_n}x{i}={as09_n * i}");
+            }
+            
         }
 
         /*
@@ -426,8 +489,14 @@ namespace Assignment
         public int as10_n;
         public void AS10_FindSummationFromZeroToNUsingWhileLoop()
         {
-            throw new NotImplementedException();
-
+            int sum = 0;
+            int i = 1;
+            while (i <= as10_n)
+            {
+                sum += i;
+                i++;
+            }
+            Debug.Log($"ผลรวมของ n จาก 1 ถึง {as10_n} คือ {sum}");
         }
 
         /*
@@ -453,8 +522,11 @@ namespace Assignment
         public GameObject as11_enemyPrefab;
         public void AS11_SpawnEnemies()
         {
-            throw new NotImplementedException();
-        }
+            for (int i = 0; i < as11_enemyHPs.Length; i++)
+            {
+                Instantiate(as11_enemyPrefab, new Vector2(i + 1, 0), transform.rotation);
+                Debug.Log($"new enemy at position x={i + 1}");
+            }        }
 
         /*
          * จงเขียนโปรแกรมเพื่อนับเวลา (Coroutine)
@@ -466,8 +538,14 @@ namespace Assignment
         public float as12_countTime;
         public IEnumerator AS12_CountTime()
         {
-            throw new NotImplementedException();
-        }
+            float timer = 0f;
+            while (timer < as12_countTime)
+            {
+                timer += Time.deltaTime;
+                Debug.Log($"timer: {timer:F2}");
+                yield return null;
+            }
+            Debug.Log($"End timer: {as12_countTime}");        }
 
         /*
          * ให้นักศึกษาเขียนโปรแกรมเพื่อหาผลรวมของตัวเลขใน Row (แถว)
@@ -524,7 +602,14 @@ namespace Assignment
         public void AS13_SumOfNumbersInRow()
         {
             var matrix = as13_matrix.Get2DArray();
-            throw new NotImplementedException();
+            int sum = 0;
+            int cols = matrix.GetLength(1);
+
+            for (int col = 0; col < cols; col++)
+            {
+                sum += matrix[as13_row, col];
+            }
+            Debug.Log(sum);
         }
 
         /*
@@ -580,8 +665,14 @@ namespace Assignment
         public void AS14_SumOfNumbersInColumn()
         {
             var matrix = as14_matrix.Get2DArray();
-            throw new NotImplementedException();
-        }
+            int sum = 0;
+            int rows = matrix.GetLength(0);
+
+            for (int row = 0; row < rows; row++)
+            {
+                sum += matrix[row, as14_column];
+            }
+            Debug.Log(sum);        }
 
         /*
          * จงเขียนโปรแกรมใน C# เพื่อแสดงวิธีคิดของการสร้างแผนที่ 3 เหลี่ยม โดยใช้ nested loop
@@ -630,7 +721,15 @@ namespace Assignment
         public int as15_size;
         public void AS15_MakeTheTriangle()
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= as15_size; i++)
+            {
+                string rowStars = "";
+                for (int j = 1; j <= i; j++)
+                {
+                    rowStars += "*";
+                }
+                Debug.Log(rowStars);
+            }
         }
 
         /*
@@ -659,7 +758,20 @@ namespace Assignment
          */
         public void AS16_MultiplicationTableOf_2_3_and_4()
         {
-            throw new NotImplementedException();
+            for (int i = 1; i <= 12; i++)
+            {
+                string rowLine = "";
+                for (int j = 2; j <= 4; j++)
+                {
+                    rowLine += $"{j}x{i}={j * i}";
+                    if (j < 4)
+                    {
+                        rowLine += "\t";
+                    }
+                }
+
+                Debug.Log(rowLine);
+            }
         }
 
         #endregion
